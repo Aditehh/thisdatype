c55757d1acf373c9cbcc7f35ecb218027ce0c0d6
aditehh <aditya.aryal888@gmail.com>
2025-11-29 20:55:15 +0545

    thisthebesttypescript

--- src/abstract.ts
+++ src/abstract.ts
@@ -3,14 +3,31 @@ abstract class clickpicture {
         public cameramode: string,
         public filter: string
     ) { }
+
+
+    abstract getsepia(): void
+    getreeltime(): number {
+        return 8
+    }
 }
 
 
 
 
 class instagram extends clickpicture {
+    constructor(
+        public cameramode: string,
+        public filter: string,
+        public burst: number
+    ) {
+        super(cameramode, filter)
+    }
+
+    getsepia(): void {
+        console.log("sepia")
+    }
 
 }
 
 
-const hc = new instagram("potrait", "vintage")
+const hc = new instagram("potrait", "vintage",3)
