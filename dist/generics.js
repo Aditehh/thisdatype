960937e7e9cebafc4f6469b73180fef653f5648c
aditehh <aditya.aryal888@gmail.com>
2025-11-30 20:41:52 +0545

    this

--- dist/generics.js
+++ dist/generics.js
@@ -1,4 +1,21 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //generics in ts called typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree(3)
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "samsung", type: 55 });
+// how array work in generics
 //# sourceMappingURL=generics.js.map
--- src/generics.ts
+++ src/generics.ts
@@ -1,1 +1,33 @@
 //generics in ts called typescript
+
+const score: Array<number> = []
+const names: Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number {
+    return val
+}
+
+
+function identityTwo(val: any): any {
+    return val
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+// identityThree(3)
+
+function identityFour<T>(val: T) {
+    return val
+}
+
+interface Bottle {
+    brand: string,
+    type: number
+}
+
+identityFour<Bottle>({ brand: "samsung", type: 55 })
+
+
+// how array work in generics
